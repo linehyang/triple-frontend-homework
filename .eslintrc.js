5c01e07e3be6de247f2a57c91ac7b27cb97c6bdd
linehyang <aldzkwp1912@gmail.com>
2022-06-28 03:46:36 +0900

    Chore/install styled-component & global css update

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,20 @@
 const createConfig = require('@titicaca/eslint-config-triple/create-config')
 
-module.exports = createConfig({ type: 'frontend', project: './tsconfig.json' })
+const { extends: extendConfigs, overrides } = createConfig({
+  type: 'frontend',
+  project: './tsconfig.json',
+})
+
+module.exports = {
+  extends: [
+    ...extendConfigs,
+    // 확장할 규칙 이름...
+  ],
+  overrides: [
+    ...overrides,
+    // 특정 파일 대상 규칙...
+  ],
+  rules: {
+    // 개별 규칙
+  },
+}
